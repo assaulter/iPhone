b6130ebe338570b778e14cfef53e16dff8311f91
Kazuki Kubo <kz@assaulter.org>
2013-07-15 11:37:14 +0900

    add AVFoundationSample Signed-off-by: Kazuki Kubo <kz@assaulter.org>

---
+++ AVfoundationSample/AVfoundationSample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AVfoundationSample
+//
+//  Created by kubo_kazuki on 13/07/15.
+//  Copyright (c) 2013年 kubo_kazuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
