632d38212ba9fe520d690ea823df1984a8f24a5d
久保 和貴 <k-kubo@biglobe.co.jp>
2013-09-16 01:57:08 +0900

    update Signed-off-by: 久保 和貴 <k-kubo@biglobe.co.jp>

---
+++ SoftSanteaTimer/SoftSanteaTimer/RequestFormViewController.h
@@ -0,0 +1,13 @@
+//
+//  RequestFormViewController.h
+//  SoftSanteaTimer
+//
+//  Created by KazukiKubo on 2013/09/15.
+//  Copyright (c) 2013年 kubo_kazuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RequestFormViewController : UIViewController
+
+@end
---
+++ SoftSanteaTimer/SoftSanteaTimer/RequestFormViewController.m
@@ -0,0 +1,39 @@
+//
+//  RequestFormViewController.m
+//  SoftSanteaTimer
+//
+//  Created by KazukiKubo on 2013/09/15.
+//  Copyright (c) 2013年 kubo_kazuki. All rights reserved.
+//
+
+#import "RequestFormViewController.h"
+
+@interface RequestFormViewController ()
+
+@end
+
+@implementation RequestFormViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    self.title = @"Request Form";
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
